16b13b2d4adafeb74d084a477d5f517243038340
Iury Barbosa <iury.alves@ccc.ufcg.edu.br>
2024-06-10 00:16:52 -0300

    [ADD] model: ItemPedido

--- src/main/java/com/vendas/domain/model/ItemPedido.java
+++ src/main/java/com/vendas/domain/model/ItemPedido.java
@@ -1,6 +1,41 @@
 package com.vendas.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table( name = "itemPedido" )
 public class ItemPedido {
-    
+    @Id
+    @JsonProperty("id")
+    @Column(name = "pk_id_item_peido")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    @Column(name = "quantidade")
+    private Integer quantidade;
 }
